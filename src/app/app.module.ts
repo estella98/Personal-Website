07c05ad3231602348335aa6d8c3480858b0646a8
unknown <k274chen@edu.uwaterloo.ca>
2018-09-30 14:43:40 -0400

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './body/home/home.component';
+import { ResumeComponent } from './body/resume/resume.component';
+import { AboutComponent } from './body/about/about.component';
+import { ContactComponent } from './body/contact/contact.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRouter: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'resume', component: ResumeComponent},
+  { path: 'contact', component: ContactComponent}
+]
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    HomeComponent,
+    ResumeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRouter)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
